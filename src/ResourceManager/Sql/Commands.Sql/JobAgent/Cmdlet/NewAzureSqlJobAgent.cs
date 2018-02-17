ec296b8f929bc3e79b38df90b52492844564687c
Jared Moore <jaredmoo@microsoft.com>
2018-02-17 00:35:04 -0800

    Catch new CloudException

--- src/ResourceManager/Sql/Commands.Sql/JobAgent/Cmdlet/NewAzureSqlJobAgent.cs
+++ src/ResourceManager/Sql/Commands.Sql/JobAgent/Cmdlet/NewAzureSqlJobAgent.cs
@@ -12,10 +12,10 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-using Hyak.Common;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using Microsoft.Rest.Azure;
 
 namespace Microsoft.Azure.Commands.Sql.JobAgent.Cmdlet
 {
